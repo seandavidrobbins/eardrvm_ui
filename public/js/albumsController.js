82e2734e6d6cef8eb8e707360bbad1e704d12760
estermer <eric.stermer@gmail.com>
2016-11-03 22:00:20 -0400

    Enabled file upload to rails server via ng-file-upload
    downloaded included and injected ngFileUpload
    
    Create an upload function in albums controller and sent data from form to rails app
    
    created form with ngf directives to upload the file in the albums view

--- public/js/albumsController.js
+++ public/js/albumsController.js
@@ -2,8 +2,25 @@
   angular.module('nitpic')
     .controller('albumsController', albumsController);
 
-  function albumsController($http){
+  albumsController.$inject = ['$scope', '$http', 'Upload', '$timeout'];
 
+  function albumsController($scope, $http, Upload, $timeout){
+    var url = 'http://localhost:3000';
+
+    //need an initial http to get all the photos for an album
+
+    $scope.uploadPhoto = function(image){
+      console.log("Uploading...");
+      image.upload = Upload.upload({
+        url: url + '/photos',
+        data: {photo: {title: $scope.title, image: image}}
+      })
+      .then(function(response){
+          console.log(response);
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    };
   }
 })()
-console.log("albumsController.js");

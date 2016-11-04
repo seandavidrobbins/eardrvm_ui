47ff84940f7a30229284c1cfebcb24d8eb6c6321
Aaron Ballard <mraballard@gmail.com>
2016-11-04 13:40:03 -0300

    deleted controller files, kept main

--- public/js/commentsController.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  angular.module('nitpic')
-    .controller('commentsController', commentsController);
-
-  function commentsController($http){
-
-  }
-})()
-console.log("commentsController.js");
--- public/js/photosController.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  angular.module('nitpic')
-    .controller('photosController', photosController);
-
-  function photosController($http){
-
-  }
-})()
-console.log("photosController.js");
